31dc446976054261c8cfdc163f641431c1f20803
Ed Wright <ew_wrightiii@live.com>
2018-01-20 23:53:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -88,6 +88,11 @@ function createRock(x) {
      rock.style.top = `${top+=2}px`;
       if(checkCollision(rock)){
         endGame();
+        if (top < GAME_HEIGHT){
+       window.requestAnimationFrame(moveRock);
+     } else {
+       rock.remove();
+     }
       }
     /**
      * Otherwise, if the rock hasn't reached the bottom of
