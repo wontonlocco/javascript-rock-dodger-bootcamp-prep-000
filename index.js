61e70914e4eb1a1be552827e3c3aa46b5601bbc7
Ed Wright <ew_wrightiii@live.com>
2018-01-20 23:50:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ function createRock(x) {
      * If a rock collides with the DODGER,
      * we should call endGame()
      */
+     rock.style.top = `${top2+=2}px`;
       if(checkCollision(rock)){
         endGame();
       }
