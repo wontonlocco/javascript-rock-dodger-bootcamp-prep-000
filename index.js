8268fc6e5798e71054c880c752ed1fe9fdf3d2c5
Ed Wright <ew_wrightiii@live.com>
2018-01-21 00:17:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -170,7 +170,7 @@ function moveDodgerRight() {
    window.requestAnimationFrame(function() {
     const left = positionToInteger(DODGER.style.left)
     if (left<360) {
-      DODGER.style.left = `${right - 4}px`
+      DODGER.style.left = `${left + 4}px`
     }
   });
   }
